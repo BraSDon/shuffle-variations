1e5ee8198f1e26b208638459d69a532a1b6b5b43
Brandon <brandon@schuehlein.eu>
2023-05-04 14:06:43 +0200

    Write outline of main

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def main():
+    # 1. Parse arguments
+    # 2. Setup distributed training (if applicable)
+    # 2a. Set seeds
+    # 3. Setup dataloaders
+    # 4. Setup model
+    # 5. Setup training_objects (criterion, optimizer, lr_scheduler)
+    # 6. Setup trainer
+    # 7. Run training
+    # 8. Store results
+    pass
