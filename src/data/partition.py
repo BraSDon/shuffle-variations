84cc62b08107ccb14b772f32b43ddae15cbebe38
Brandon <brandon@schuehlein.eu>
2023-05-04 13:38:26 +0200

    Add partitioners (formerly known as chunkers)

---
+++ src/data/partition.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod, ABCMeta
+
+
+class Partitioner(metaclass=ABCMeta):
+    @abstractmethod
+    def partition(self, world_size, indices):
+        pass
+
+
+class SequentialPartitioner(Partitioner):
+    def partition(self, world_size, indices):
+        len_per_rank = len(indices) // world_size
+        return [
+            indices[i * len_per_rank : (i + 1) * len_per_rank]
+            for i in range(world_size)
+        ]
+
+
+class StepPartitioner(Partitioner):
+    def partition(self, world_size, indices):
+        return [indices[rank::world_size] for rank in range(world_size)]
