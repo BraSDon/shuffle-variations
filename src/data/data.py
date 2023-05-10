424d7066332f22f9b155a9501915e6fd629b5568
Brandon <brandon@schuehlein.eu>
2023-05-10 12:15:18 +0200

    Implement data.py to create dataloaders
    The available datasets need to be
    described in the system-config.yaml file.
    To add new datasets, only the configuration
    file needs to be adjusted. However,
    there might be load-functions that do not work
    with my implementation and require manual
    adjustment in data.py

---
+++ src/data/data.py
@@ -0,0 +1,87 @@
+from torch.utils.data import DataLoader, Dataset
+from torchvision.transforms import transforms
+
+
+# TODO: Test this class!
+class MyDataset:
+    """Class for handling datasets and returning dataloaders."""
+
+    def __init__(
+        self, name: str, path: str, transformations: dict, load_function: dict
+    ):
+        self._name = name
+        self._path = path
+
+        self._transform = self.__extract_transform(transformations)
+        self._load_function, self._lf_type = self.__extract_load_function(load_function)
+
+    def get_dataloaders(
+        self, batch_size, sampler, num_workers, **kwargs
+    ) -> dict[str, DataLoader]:
+        train, test = self.__get_datasets()
+        train_loader = DataLoader(
+            train,
+            batch_size=batch_size,
+            sampler=sampler,
+            num_workers=num_workers,
+            **kwargs,
+        )
+        test_loader = DataLoader(
+            test,
+            batch_size=batch_size,
+            sampler=sampler,
+            num_workers=num_workers,
+            **kwargs,
+        )
+        return {"train": train_loader, "test": test_loader}
+
+    def __get_datasets(self) -> tuple[Dataset, Dataset]:
+        train_path = self._path + "/train"
+        test_path = self._path + "/test"
+
+        # NOTE: Adjust if load-function requires it.
+        if self._lf_type == "built-in":
+            train_dataset = self._load_function(train_path, self._transform, train=True)
+            test_dataset = self._load_function(test_path, self._transform, train=False)
+        elif self._lf_type == "generic":
+            train_dataset = self._load_function(train_path, self._transform)
+            test_dataset = self._load_function(test_path, self._transform)
+        else:
+            raise ValueError(f"Unknown load function type: {self._lf_type}")
+        return train_dataset, test_dataset
+
+    @staticmethod
+    def __extract_transform(transformations: dict) -> transforms.Compose:
+        transform_list = []
+        for t in transformations:
+            transform_class = getattr(transforms, t["name"])
+            kwargs = t["kwargs"] if "kwargs" in t else {}
+            transform_list.append(transform_class(**kwargs))
+        return transforms.Compose(transform_list)
+
+    @staticmethod
+    def __extract_load_function(load_function: dict) -> tuple[callable, str]:
+        module, lf_type, name = (
+            load_function["module"],
+            load_function["type"],
+            load_function["name"],
+        )
+
+        module = __import__(module)
+        load_function = getattr(module, name)
+        return load_function, lf_type
+
+    @property
+    def get_transforms(self) -> transforms.Compose:
+        """Returns a Compose object of transforms to be applied to the dataset."""
+        return self._transform
+
+    @property
+    def path_to_dataset(self) -> str:
+        """Returns the path to the dataset."""
+        return self._path
+
+    @property
+    def name(self) -> str:
+        """Returns the name of the dataset."""
+        return self._name
