cf28fdc29fed1abbb8a138835e3e73938f75034e
Brandon <brandon@schuehlein.eu>
2023-05-15 17:08:45 +0200

    Add basic sbatch script for HAICORE

---
+++ slurm/haicore.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+#SBATCH --job-name=haicore-default
+#SBATCH --output=haicore-default.out
+#SBATCH --time=01:00:00
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --gres=gpu:full:4
+
+module load compiler/gnu/11
+module load devel/cuda/11.8
+module load mpi/openmpi/4.1
+
+source /hkfs/work/workspace/scratch/tz6121-paper/paper/venv/bin/activate
+
+srun python -u ../src/main.py
