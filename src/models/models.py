12d704ed691aa188b076bd7751a03742575aca9f
Brandon <brandon@schuehlein.eu>
2023-05-25 13:13:15 +0200

    Add simple ANN model for SUSY

--- src/models/models.py
+++ src/models/models.py
@@ -30,3 +30,27 @@ class DummyModel(nn.Module):
         x = self.relu3(x)
         x = self.linear2(x)
         return x
+
+
+class ANN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(18, 200)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(200, 200)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(200, 200)
+        self.relu3 = nn.ReLU()
+        self.fc4 = nn.Linear(200, 2)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        x = self.relu3(x)
+        x = self.fc4(x)
+        x = self.softmax(x)
+        return x
