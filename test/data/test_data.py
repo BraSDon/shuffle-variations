eda3d0383e9fe204c8e36a700c6873ff400885c2
Brandon <brandon@schuehlein.eu>
2023-08-29 15:38:15 +0200

    Refactor test_data.py to be executable by a single process

--- test/data/test_data.py
+++ test/data/test_data.py
@@ -1,6 +1,5 @@
 import sys
 import unittest
-import torch.distributed as dist
 import torch
 from unittest.mock import MagicMock, patch
 from torch.utils.data import DataLoader
@@ -9,12 +8,8 @@ from torchvision.datasets import CIFAR10
 
 sys.path.insert(0, sys.path[0] + "/../../")
 from src.data.data import MyDataset
-from src.main import setup_distributed_training
 
 
-# NOTE: All tests are skipped (return without testing) if distributed training
-# fails to initialize. To initialize distributed training, run the following command:
-# torchrun --nproc_per_node=4 test/data/test_data.py
 class TestMyDataset(unittest.TestCase):
     data_path = "../../data"
 
@@ -24,14 +19,12 @@ class TestMyDataset(unittest.TestCase):
         CIFAR10(root=cls.data_path, train=True, download=True)
         CIFAR10(root=cls.data_path, train=False, download=True)
 
-        try:
-            setup_distributed_training("local", "29500")
-        except:
-            return
-        dist.barrier()
-
-    def setUp(self) -> None:
+    # patch copy_dataset_to_tmp to avoid copying the dataset to tmp.
+    @patch("src.data.data.MyDataset.copy_dataset_to_tmp")
+    def setUp(self, copy_mock) -> None:
         """Initialize the dataset."""
+        copy_mock.return_value = self.data_path
+
         train_transformations = [
             {"name": "ToTensor", "kwargs": {}},
         ]
